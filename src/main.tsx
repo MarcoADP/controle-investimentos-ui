fc99cab730f5bd6c2ad3adbe3652957402d7f17e
Marco Aurélio Deoldoto Paulino <marco.ad.paulino@gmail.com>
2025-05-14 11:46:31 -0300

    Correção para aguarda o mock antes de iniciar a aplicação

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+async function prepare() {
+  if (import.meta.env.DEV) {
+    const { worker } = await import('./mocks/browser');
+    await worker.start({
+      onUnhandledRequest: 'warn',
+    });
+  }
 
-console.log('1')
-if (import.meta.env.DEV) {
-  console.log('2')
-  const { worker } = await import('./mocks/browser');
-  await worker.start({
-    onUnhandledRequest: 'bypass',
-  });
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
-console.log('3')
+
+
+
+prepare();
