bca5078206197700543559fd86f9fa2faaac821d
Marco Aurélio Deoldoto Paulino <marco.ad.paulino@gmail.com>
2025-05-15 19:57:41 -0300

    Adicionar links úteis no read.me

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 
 async function prepare() {
+  /*
   if (import.meta.env.DEV) {
     const { worker } = await import('./mocks/browser');
     await worker.start({
       onUnhandledRequest: 'warn',
     });
   }
+  */
 
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
